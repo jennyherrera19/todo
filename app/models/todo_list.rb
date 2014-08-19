cab551ae55028b35be19b506370b01949ec7f559
Jenny Herrera <jennyherrera.19@gmail.com>
2014-08-18 22:23:13 -0700

    Created todo app

---
+++ app/models/todo_list.rb
@@ -0,0 +1,4 @@
+class TodoList < ActiveRecord::Base
+  has_many :todo_items, dependent: :destroy
+  validates :name, presence: true
+end
---
+++ db/migrate/20140814041312_add_completed_to_todo_items.rb
@@ -0,0 +1,5 @@
+class AddCompletedToTodoItems < ActiveRecord::Migration
+  def change
+    add_column :todo_items, :completed, :boolean
+  end
+end
